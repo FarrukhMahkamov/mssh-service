183916697ed90944dea47827c24aebb70764dfe9
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-01-10 14:45:36 +0500

    Add State, Regions and Address modelan and migration

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SizeController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function(){
+Route::prefix('v1')->group(function() {
     
     Route::apiResource('products', ProductController::class);
 
