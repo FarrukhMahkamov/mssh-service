eea68f918d7c639ab0b5303f0c2ddf1c7141f7c8
FarrukhMahkamov <farruhmahkamov20@gmail.com>
2022-01-19 15:00:17 +0500

    Make observer work

--- app/Observers/ProductObserver.php
+++ app/Observers/ProductObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Cache;
 
 class ProductObserver
 {
@@ -14,50 +15,6 @@ class ProductObserver
      */
     public function created(Product $product)
     {
-        //
-    }
-
-    /**
-     * Handle the Product "updated" event.
-     *
-     * @param  \App\Models\Product  $product
-     * @return void
-     */
-    public function updated(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Handle the Product "deleted" event.
-     *
-     * @param  \App\Models\Product  $product
-     * @return void
-     */
-    public function deleted(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Handle the Product "restored" event.
-     *
-     * @param  \App\Models\Product  $product
-     * @return void
-     */
-    public function restored(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Handle the Product "force deleted" event.
-     *
-     * @param  \App\Models\Product  $product
-     * @return void
-     */
-    public function forceDeleted(Product $product)
-    {
-        //
+        Cache::forget('products');
     }
 }
