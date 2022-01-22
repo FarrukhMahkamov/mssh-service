ec8861fe2074fe3d21ebf50c7335b4c9203a9eca
Turdialiyev <turdialiyevgolibjon0226@gmail.com>
2022-01-22 12:59:11 +0500

    Add Table for Product_creams and Delivery_leftover

--- database/migrations/2022_01_22_075002_create_product_creams_table.php
+++ database/migrations/2022_01_22_075002_create_product_creams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateProductCreamsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('product_creams', function (Blueprint $table) {
             $table->id();
-            $table->string('date');
-            $table->string('client');
-            $table->string('agent');
-            $table->string('duty');
-            $table->string('duty1');
+            $table->string('data');
+            $table->string('document');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('product_creams');
     }
 }
